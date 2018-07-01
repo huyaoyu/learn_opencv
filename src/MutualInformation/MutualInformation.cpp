32540c8c777f6d03d0504a72f0625026f4e36e86
Yaoyu Hu <yyhu_live@outlook.com>
2018-07-01 17:42:23 -0400

    RC.

--- src/MutualInformation/MutualInformation.cpp
+++ src/MutualInformation/MutualInformation.cpp
@@ -65,6 +65,26 @@ void flush_small_positive_values(cv::InputOutputArray _m, T limit, T v = -1)
 	}
 }
 
+static int put_initial_disparity_map(int rows, int cols, cv::OutputArray _D, float val, int type = CV_32FC1)
+{
+	if ( type == CV_32FC1 || type == CV_32F ||
+		 type == CV_64FC1 || type == CV_64F )
+	{
+		_D.create( rows, cols, type );
+
+		cv::Mat D = _D.getMat();
+
+		D = val;
+
+		return 0;
+	}
+	else
+	{
+		std::cout << "Error. Type must be one of the followings: CV_32FC1, CV_32F, CV_64FC1, CV_64F." << std::endl;
+		return -1;
+	}
+}
+
 /***
  * \param _src Must be using the type of CV_8UC1.
  */
@@ -92,9 +112,12 @@ static int warp(cv::InputArray _src, cv::InputArray _D, cv::OutputArray _dst)
 
 	// Allocate memory for _dst.
 	_dst.create( src.rows, src.cols, CV_8UC1 );
+	cv::Mat dst = _dst.getMat();
 
-	int d = 0;
-	int i2 = 0;
+	src.copyTo(dst);
+
+	int d  = 0; // Disparity.
+	int j2 = 0; // Newly projected index.
 
 	for ( int i = 0; i < src.rows; ++i )
 	{
@@ -107,31 +130,31 @@ static int warp(cv::InputArray _src, cv::InputArray _D, cv::OutputArray _dst)
 			{
 				d = 0;
 			}
-			else if ( j - d <= 0 )
+			else if ( j + d >= src.cols  )
 			{
-				d = j;
+				d = src.cols - j;
 			}
 
-			i2 = j - d;
+			// New index.
+			j2 = j + d;
 
 			// Assign
-
+			dst.at<uchar>(i, j2) = src.at<uchar>(i, j);
 		}
 	}
 
-
+	return 0;
 }
 
 static void mutual_information(cv::InputArray _s1, cv::InputArray _s2, cv::OutputArray _dst, int intensityBins = 256)
 {
-	// Get the Mat objects.
-
+	// === Get the Mat objects. ===
 	cv::Mat s1 = _s1.getMat();
 	cv::Mat s2 = _s2.getMat();
 
 	cv::Mat histDst;
 
-	// Compute the histogram.
+	// === Compute the histogram. ===
 	cv::Mat inputMats[] = {s1, s2};
 	// Prepare arguments for cv::calcHist().
 	int channels[] = { 0 ,1 };
@@ -147,28 +170,28 @@ static void mutual_information(cv::InputArray _s1, cv::InputArray _s2, cv::Outpu
 
 	histDst *= f;
 
-	// Gaussian convolution with GaussianBlur.
+	// === Gaussian convolution with GaussianBlur. ===
 	cv::Mat GBDst;
 //	cv::GaussianBlur( histDst, GBDst, cv::Size( 7, 7 ), 0, 0, cv::BORDER_CONSTANT );
 	cv::GaussianBlur( histDst, GBDst, cv::Size( 7, 7 ), 0, 0 );
 
-	// Logarithm.
+	// === Logarithm. ===
 	cv::Mat logDst;
 	// Direct log() without any modification for the zero value in GBDst will cause problem.
 	// The current version of OpenCV, 3.4.1, behaves differently with the 2.x version.
 	// cv::log() gaves undefined value if the input is negative, zero, NaN and Inf.
-
 	flush_small_positive_values<Run_MutualInformation::real>(GBDst, 1e-20);
 	cv::log( GBDst, logDst );
 	logDst *= -1;
 
-	// Gaussian convolution with GaussianBlur, again.
+	// === Gaussian convolution with GaussianBlur, again. ===
 //	cv::GaussianBlur( logDst, GBDst, cv::Size( 7, 7), 0, 0, cv::BORDER_CONSTANT );
 	cv::GaussianBlur( logDst, GBDst, cv::Size( 7, 7), 0, 0 );
 
-	// Divide by the size of the image.
+	// === Divide by the size of the image. ===
 	GBDst *= f;
 
+	// === Preserve the result. ===
 	_dst.assign(GBDst);
 
 	std::cout << "Test output." << std::endl;
@@ -198,23 +221,44 @@ Runnable::RES_t Run_MutualInformation::run(void)
 	cv::cvtColor(srcImgs[1], greyImgs[1], cv::COLOR_BGR2GRAY, 1);
 
 	// Show the grey images.
-	namedWindow("Greyscale image left", cv::WINDOW_NORMAL );
+	cv::namedWindow("Greyscale image left", cv::WINDOW_NORMAL );
 	cv::imshow("Greyscale image left", greyImgs[0] );
 
 	std::cout << "Channels in left image is " << greyImgs[0].channels() << std::endl;
 
 //	std::cout << greyImgs[0] << std::endl;
 
-	namedWindow("Greyscale image right", cv::WINDOW_NORMAL );
+	cv::namedWindow("Greyscale image right", cv::WINDOW_NORMAL );
 	cv::imshow("Greyscale image right", greyImgs[1] );
 
 	std::cout << "Channels in right image is " << greyImgs[1].channels() << std::endl;
 
+	// Warp operation.
+	cv::Mat warped; // Warped image.
+	cv::Mat D;      // Disparity.
 	cv::Mat mi;
-	mutual_information( greyImgs[0], greyImgs[1], mi );
+
+	if ( 0 != put_initial_disparity_map( greyImgs[0].rows, greyImgs[0].cols, D, 300.0, CV_32FC1 ) )
+	{
+		std::cout << "Error!" << std::endl;
+		goto ERROR_POINT;
+	}
+
+	if ( 0 != warp<Run_MutualInformation::real>( greyImgs[1], D, warped ) )
+	{
+		std::cout << "Error!" << std::endl;
+		goto ERROR_POINT;
+	}
+
+	cv::namedWindow("Warped image", cv::WINDOW_NORMAL);
+	cv::imshow("Warped image", warped);
+
+	mutual_information( greyImgs[0], warped, mi );
 
 	show_floating_point_number_image(mi, "mi");
 
+
+ERROR_POINT:
 	cv::waitKey(0);
 
 	this->show_footer();
