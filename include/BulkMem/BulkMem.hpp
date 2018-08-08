ca6db8ae70f522b557670e9892db69c26d600f94
yyhu <yyhu_live@outlook.com>
2018-08-08 13:05:46 -0400

    Edit BulkMem. Could not compile.

--- include/BulkMem/BulkMem.hpp
+++ include/BulkMem/BulkMem.hpp
@@ -17,6 +17,36 @@
 namespace locv
 {
 
+template<class _T>
+class BulkMem
+{
+public:
+    typedef unsigned long i_t;
+
+public:
+    BulkMem(i_t nDim, i_t* dim, bool faked = false);
+    ~BulkMem();
+
+    _T* get(void);
+
+    bool is_faked(void);
+
+public:
+    _T* ptr;
+
+protected:
+    i_t mSize;
+    i_t mNDim;
+    i_t* mDim;
+    bool mFaked;
+
+    static i_t TOTAL_SIZE;
+
+private:
+    BulkMem(const BulkMem& bm) { }    
+    BulkMem(BulkMem& bm) { }    
+};
+
 }
 
 #endif /* INCLUDE_BULKMEM_BULKMEM_HPP_ */
--- src/BulkMem/BulkMem.cpp
+++ src/BulkMem/BulkMem.cpp
@@ -3,4 +3,49 @@
 
 using namespace locv;
 
+template<typename _T> BulkMem<_T>::i_t BulkMem<_T>::TOTAL_SIZE = 0;
 
+template<typename _T>
+BulkMem<_T>::BulkMem(i_t nDim, i_t* dim, bool faked)
+{
+    mNDim = nDim;
+    mDim = new i_t[mNDim];
+    mSize = 1;
+    for ( i_t i = 0; i < mNDim; ++i )
+    {
+        mSize *= dim[i];
+        mDim[i] = dim[i];
+    }
+
+    mFaked = faked;
+    if ( false == mFaked )
+    {
+        ptr = new _T[ mSize ];
+    }
+}
+
+template<typename _T> 
+BulkMem< _T >::~BulkMem()
+{
+    if ( ptr != NULL && false == mFaked )
+    {
+        delete [] ptr; ptr = NULL;
+    }
+
+    if ( mDim != NULL )
+    {
+        delete [] mDim; mDim = NULL;
+    }
+}
+
+template<typename _T>
+_T* BulkMem<_T>::get(void)
+{
+    return ptr;
+}
+
+template<typename _T>
+bool BulkMem<_T>::is_faked(void)
+{
+    return mFaked;
+}
